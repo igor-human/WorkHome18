252ec59a35b64edc2039d68553fd5118b7bc4206
Игорь Коржан <igor.human@gmail.com>
2024-10-24 22:11:32 +0300

    Добавьте файлы проекта.

---
+++ Task01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MyCustomDictionary;  // Простір імен з бібліотеки класів
+
+namespace MyDictionaryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створення екземпляра MyDictionary
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+
+            // Додавання елементів
+            myDictionary.Add("One", 1);
+            myDictionary.Add("Two", 2);
+            myDictionary.Add("Three", 3);
+
+            // Використання індексатора для отримання значення за ключем
+            Console.WriteLine("Значення для ключа 'Two': " + myDictionary["Two"]);
+
+            // Отримання загальної кількості елементів
+            Console.WriteLine("Загальна кількість елементів: " + myDictionary.Count);
+
+            // Перебір елементів за допомогою foreach
+            Console.WriteLine("Елементи MyDictionary:");
+            foreach (var kvp in myDictionary)
+            {
+                Console.WriteLine($"Ключ: {kvp.Key}, Значення: {kvp.Value}");
+            }
+        }
+    }
+}
+
+
