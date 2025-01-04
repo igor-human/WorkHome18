0c5044b0755305e6c4ca6a94a207d621985044c2
Игорь Коржан <igor.human@gmail.com>
2025-01-04 20:12:51 +0200

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿using AssemblyA;
+
+namespace AssemblyB
+{
+    public class DerivedClass : BaseClass
+    {
+        public void CallBasePublicMethod()
+        {
+            // Виклик публічного методу базового класу
+            PublicMethod();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Інстанціюємо похідний клас
+            DerivedClass derived = new DerivedClass();
+
+            // Викликаємо метод базового класу через похідний клас
+            derived.CallBasePublicMethod();
+        }
+    }
+}
